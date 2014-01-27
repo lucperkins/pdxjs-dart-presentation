38d43895b5f66a97e8398486015bf7ecd79d184e
Luc Perkins <lucperkins@gmail.com>
2014-01-27 14:18:48 -0800

    cleanup

--- web/app.dart
+++ web/app.dart
@@ -3,11 +3,5 @@ library app;
 import 'package:polymer/polymer.dart';
 
 void main() {
-  //initPolymer();
-  
-  try {
-    print(1/0);
-  } catch(e) {
-    print(e.runtimeType);
-  }
+  initPolymer();
 }
