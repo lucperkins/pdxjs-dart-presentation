0a277f71b2f78813b2178215787356e3ea221834
Luc Perkins <lucperkins@gmail.com>
2014-02-04 14:16:27 -0800

    small changes

--- build.dart
+++
@@ -1,7 +0,0 @@
-import 'package:polymer/builder.dart';
-
-void main() {
-  build(
-    entryPoints: ['web/app.dart', 'server/server.dart']
-  );
-}
--- web/app.dart
+++ web/app.dart
@@ -1,34 +1,7 @@
 library app;
 
-import 'dart:html';
 import 'package:polymer/polymer.dart';
-import 'package:logging/logging.dart';
-import 'package:route/client.dart';
-
 
 void main() {
-  Logger.root.level = Level.INFO;
-  Logger log = new Logger('main');
-
   initPolymer();
-  
-  List<UListElement> uls = querySelectorAll('ul');
-  uls.forEach((UListElement ul) {
-    ul.innerHtml = '...';
-  });
-}
-
-void serve() {
-  String rootPath = window.location.pathname;
-  
-  final UrlPattern homeUrl = new UrlPattern(r'${rootPath}');
-  final UrlPattern otherUrl = new UrlPattern(r'/other');
-  
-  final Router router = new Router(useFragment: true)
-    ..addHandler(homeUrl, showHomePage)
-    ..listen();  
-}
-
-void showHomePage(String path) {
-  
 }
